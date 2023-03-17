155ba64df619ad81cd8a933566b2faaff5282dbf
afzal mahmud <afzzalmahmud@gmail.com>
2023-03-17 07:25:39 -0700

    added necessary file sructure and react-icons dependency

---
+++ src/Containers/index.js
@@ -0,0 +1,8 @@
+// Hear we add export deafult to reduce messy code style when necessery
+
+export {deafult as Blog} from './blog/Blog'
+export {deafult as Features} from './features/Features'
+export {deafult as Footer} from './footer/Footer'
+export {deafult as Header} from './header/Header'
+export {deafult as Possibility} from './possibility/Possibility'
+export {deafult as WhatGPT3} from './whatGPT3/WhatGPT3'
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// Hear we add export deafult to reduce messy code style when necessery
+
+export {deafult as Article} from './article/Article'
+export {deafult as Brand} from './brand/Brand'
+export {deafult as CTA} from './cta/CTA'
+export {deafult as Feature} from './brand/Feature'
+export {deafult as Navbar} from './brand/Navbar'
