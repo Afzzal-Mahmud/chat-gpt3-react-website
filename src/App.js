c78aec2c1257d680a535260be9d4e3c6310e5297
afzal mahmud <afzzalmahmud@gmail.com>
2023-03-17 22:11:08 -0700

    import components and container to the file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
 import './App.css';
+
+import { Header,Blog,Features,Possibility,WhatGPT3,Footer } from './Containers';
+import { CTA,Brand,Navbar} from './components'
 
 function App() {
   return (
     <div className="App">
-     
+      {/* hero section */}
+      <div className="gradient__bg">
+          <Navbar/>
+          <Header/>
+      </div>
+
+      <Brand/>
+    {/* a simple description about chat Gpt3 */}
+      <WhatGPT3/>
+
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
