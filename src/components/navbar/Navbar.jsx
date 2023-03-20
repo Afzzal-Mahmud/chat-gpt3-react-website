2dab219b62c7e6487ec9d10eb544402cf270d1d2
afzal mahmud <afzzalmahmud@gmail.com>
2023-03-20 10:50:36 -0700

    complited navigation bar and done it's responsivness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
+import { RiMenu3Line,RiCloseLine } from 'react-icons/ri'
+import svgLogo from '../../assets/logo.svg'
+import  {NavbarLinks, NavbarButtons } from './navbarFn'
+
+
 
 const Navbar = () => {
+  const [toggleMobileMenu, setToggleMobileMenu] = useState(true)
   return (
-    <div>NavigationBar</div>
+    <div className='gpt3__navbar'>
+
+        <div className="gpt3__navbar-logo">
+          <img src={svgLogo} alt="svg logo"/>
+        </div>
+
+      <div className="gpt3__navbar-links_container">
+
+        <div className="gpt3__navbar-links">
+          <NavbarLinks/>          
+        </div>
+
+        <div className="gpt3__navbar-links_button">
+            <NavbarButtons/>
+        </div>
+
+      </div>
+
+      <div className="gpt3__navbar-mobile_menu">
+      {toggleMobileMenu
+          ? <RiMenu3Line color="#fff" size={30} onClick={() => setToggleMobileMenu(false)} />
+          : <RiCloseLine color="#fff" size={30} onClick={() => setToggleMobileMenu(true)} />}
+
+      {!toggleMobileMenu && (
+        <div className="gpt3__navbar-mobile_container scale-up-center">
+          <div className="gpt3__navbar-mobile_links">
+              <NavbarLinks/>
+          </div>
+          <div className="gpt3__navbar-mobile_buttons">
+              <NavbarButtons/>
+          </div>
+        </div>
+      )
+
+      }
+      </div>
+    </div>
   )
 }
 
