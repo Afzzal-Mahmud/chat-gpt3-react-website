0e6232332c6b0b30d7829982dd36125b294d7132
afzal mahmud <afzzalmahmud@gmail.com>
2023-03-27 17:07:14 +0600

    added the basic structure and style of whatgpt3 section

--- src/Containers/whatGPT3/WhatGPT3.jsx
+++ src/Containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Feature from '../../components/feature/Feature'
 import './whatGPT3.css'
 
 function WhatGPT3() {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className="gpt3__whatgpt3_top-content">
+        <Feature/>
+      </div>
+
+      <div className="gpt3__whatgpt3_bottom-content">
+        
+        <div className="gpt3__whatgpt3_bottom-content_heading">
+          <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+          <a href="#">Explore the library</a>
+        </div>
+
+        <div className="gpt3__whatgpt3_bottom-content_card">
+          <Feature/>
+          <Feature/>
+          <Feature/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
